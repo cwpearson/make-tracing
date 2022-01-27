fe51513b7b9bf5b5c04cbbffe13670949d006155
Carl Pearson <cwpearson@users.noreply.github.com>
2022-01-27 16:52:52 -0700

    Add nanos, tag with B and E

--- trace-make.sh
+++ trace-make.sh
@@ -5,11 +5,11 @@ export TRACE=make.trace
 # redirect fd 100 to lock file
 # exec 100>timing.lock
 
-# record the pid, the command make is running, and the start time 
-echo "$$ [$*]" `date +%s` >> $TRACE
+# record the pid, and the start time (s.ns), and the command make is running
+echo B $$ `date +%s.%N` "[$*]" >> $TRACE
 
 # the actual command make wants to run
 bash "$@"
 
-# record the pid, and the end time
-echo $$ `date +%s` >> $TRACE
+# record the pid and the end time (s.ns)
+echo E $$ `date +%s.%N` >> $TRACE
